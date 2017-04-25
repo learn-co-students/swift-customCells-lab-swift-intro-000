7259a41490441950df5fbd9db48b62dc34e6e192
Hendricus Disseldorp <hdisseldorp@gmail.com>
2017-04-25 18:28:56 +0300

    Code tableview controller and added segue to detail view

--- CustomCellMath/TableViewController.swift
+++ CustomCellMath/TableViewController.swift
@@ -9,44 +9,67 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+  
+  // an array of arrays
+  // i.e. [[1, 2, 3, 4], [5, 3, 1, 0], [5, 2, 6, 6]]
+  var numbers: [[Int]] = []
+  let reusableCell: String = "mathCell"
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    generateData()
+  }
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return numbers.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: reusableCell, for: indexPath) as! MathTableViewCell
     
-    // an array of arrays
-    // i.e. [[1, 2, 3, 4], [5, 3, 1, 0], [5, 2, 6, 6]]
-    var numbers: [[Int]] = []
+    let numberArray = numbers[indexPath.row]
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        generateData()
+    cell.firstNumberLabel.text? = String(numberArray[0])
+    cell.secondNumberLabel.text? = String(numberArray[1])
+    cell.thirdNumberLabel.text? = String(numberArray[2])
+    cell.fourthNumberLabel.text? = String(numberArray[3])
+    
+    return cell
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier != "ShowMathDetails" { return }
+    if let dest = segue.destination as? DisplayMathViewController, let indexPath = tableView.indexPathForSelectedRow {
+      dest.numbers = numbers[indexPath.row]
     }
-    
-    
-    
-    
-
-
+  }
 }
 
 
 
 // MARK: - Generating an array of Data
 extension TableViewController {
-    
-    func generateData() {
-        for _ in 1...100 {
-            var newNumberRow: [Int] = []
-            
-            for _ in 1...4 {
-                let randomNumber = randomNumberFromOneTo(50)
-                newNumberRow.append(randomNumber)
-            }
-            
-            numbers.append(newNumberRow)
-        }
+  
+  func generateData() {
+    for _ in 1...100 {
+      var newNumberRow: [Int] = []
+      
+      for _ in 1...4 {
+        let randomNumber = randomNumberFromOneTo(50)
+        newNumberRow.append(randomNumber)
+      }
+      
+      numbers.append(newNumberRow)
     }
-    
-    func randomNumberFromOneTo(_ number: Int) -> Int {
-        return Int(arc4random_uniform(UInt32(number)) + 1)
-    }
-    
-    
+  }
+  
+  func randomNumberFromOneTo(_ number: Int) -> Int {
+    return Int(arc4random_uniform(UInt32(number)) + 1)
+  }
+  
+  
 }
