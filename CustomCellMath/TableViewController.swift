8f2d6ad5d9cc5905af9e9c14d3d5a45766e8a1e7
Kelly Lougheed <kellylougheed@gmail.com>
2017-05-04 12:14:16 -0700

    Build math app

--- CustomCellMath/TableViewController.swift
+++ CustomCellMath/TableViewController.swift
@@ -19,7 +19,31 @@ class TableViewController: UITableViewController {
         generateData()
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numbers.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mathCell", for: indexPath) as! MathTableViewCell
+        let numberArr = numbers[(indexPath as NSIndexPath).row]
+        cell.firstNumberLabel?.text = String(describing: numberArr[0])
+        cell.secondNumberLabel?.text = String(describing: numberArr[1])
+        cell.thirdNumberLabel?.text = String(describing: numberArr[2])
+        cell.fourthNumberLabel?.text = String(describing: numberArr[3])
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier != "DisplayMathViewController" { return }
+        if let dest = segue.destination as? DisplayMathViewController,
+            let indexPath = tableView.indexPathForSelectedRow {
+            dest.numbers = numbers[(indexPath as NSIndexPath).row]
+        }
+    }
     
     
 
