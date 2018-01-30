a3a256d62f430c3e46eafee6bfb3048b4602b14c
Luis Fernando Gutiérrez Romo <changoluis@hotmail.com>
2018-01-29 20:39:27 -0600

    .

--- CustomCellMath/TableViewController.swift
+++ CustomCellMath/TableViewController.swift
@@ -18,15 +18,30 @@ class TableViewController: UITableViewController {
         super.viewDidLoad()
         generateData()
     }
-    
-    
-    
-    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numbers.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mathCell", for: indexPath) as! MathTableViewCell
+        let nums = numbers[indexPath.row]
 
+        cell.firstNumberLabel?.text = String(nums[0])
+        cell.secondNumberLabel?.text = String(nums[1])
+        cell.thirdNumberLabel?.text = String(nums[2])
+        cell.fourthNumberLabel?.text = String(nums[3])
+        return cell
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier != "displayMath"{return}
 
+        if let destination = segue.destination as? DisplayMathViewController, let indexPath = tableView.indexPathForSelectedRow{
+            destination.numbers = numbers[indexPath.row]
+        }
+    }
 }
-
-
 
 // MARK: - Generating an array of Data
 extension TableViewController {
@@ -47,6 +62,4 @@ extension TableViewController {
     func randomNumberFromOneTo(_ number: Int) -> Int {
         return Int(arc4random_uniform(UInt32(number)) + 1)
     }
-    
-    
 }
